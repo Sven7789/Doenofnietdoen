1a46a712d3f6410f6b583c0d9eb2efbc915984de
Sven Nieuwenhuizen <62598817+Sven7789@users.noreply.github.com>
2024-05-16 14:17:08 +0200

    Working code

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ function berekenLeeftijd() {
     var leeftijd1 = parseInt(document.getElementById("leeftijd1").value);
     var leeftijd2 = parseInt(document.getElementById("leeftijd2").value);
 
-    // Bereken de minimumleeftijd die beide personen mogen hebben om een relatie te hebben
-    var minimumLeeftijd = Math.min((leeftijd1 / 2) + 7, (leeftijd2 / 2) + 7);
+    var minleeftijd1 = leeftijd1 / 2 + 7;
+    var minleeftijd2 = leeftijd2 / 2 + 7;
 
-    // Toon de minimumleeftijd
-    document.getElementById("minimumLeeftijd").textContent = minimumLeeftijd;
-
-    // Controleer of de leeftijd van een persoon de minimumleeftijd overschrijdt en toon/verberg de melding dienovereenkomstig
-    if (leeftijd1 < minimumLeeftijd || leeftijd2 < minimumLeeftijd) {
+    if (minleeftijd1 < minleeftijd2 || minleeftijd1 > leeftijd2) {
         document.getElementById("melding").style.display = "block";
     } else {
         document.getElementById("melding").style.display = "none";
